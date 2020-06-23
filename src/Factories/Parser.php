7d265a2575103db772c576215c77c017d61b0184
grandre <andreaugustomart@gmail.com>
2020-06-23 15:52:36 -0300

    Mudanças e criação no XML de emissão

--- src/Factories/Parser.php
+++ src/Factories/Parser.php
@@ -3,39 +3,19 @@
 namespace NFePHP\NFSe\SMARAPD\Factories;
 
 use NFePHP\NFSe\SMARAPD\Make;
-use stdClass;
 use NFePHP\Common\Strings;
-use App\Http\Model\Uteis;
 
 class Parser
 {
-
-    protected $structure;
-
-    protected $make;
-
-    protected $loteRps;
-
-    protected $tomador;
-
-    protected $std;
 
     public function __construct($version = '3.0.1')
     {
 
         $ver = str_replace('.', '', $version);
 
-        $path = realpath(__DIR__ . "/../../storage/txtstructure$ver.json");
+        $path = realpath(__DIR__ . "/../../storage/txtstructure.json");
 
-        $this->std = new \stdClass();
-
-        $this->std->tomador = new \stdClass();
-
-        $this->std->prestador = new \stdClass();
-
-        $this->std->servico = array();
-
-        $this->servicos = array();
+        $this->Servico = new \stdClass();
 
         $this->structure = json_decode(file_get_contents($path), true);
 
@@ -47,14 +27,11 @@ class Parser
     public function toXml($nota)
     {
 
-        $std = $this->array2xml($nota);
+        $this->array2xml($nota);
 
-        if ($this->make->getXML($this->std)) {
+        if ($this->make->monta()) {
 
-            echo $this->make->getXML($this->std);
-            die;
-
-            return $this->make->getXML($this->std);
+            return $this->make->getXML();
         }
 
         return null;
@@ -63,20 +40,25 @@ class Parser
     protected function array2xml($nota)
     {
 
-        $obj = [];
-
         foreach ($nota as $lin) {
 
             $fields = explode('|', $lin);
 
-            $struct = $this->structure[strtoupper($fields[0])];
+            if (empty($fields)) {
+                continue;
+            }
 
-            $std = $this->fieldsToStd($fields, $struct);
+            $metodo = strtolower(str_replace(' ', '', $fields[0])) . 'Entity';
 
-            $obj = (object) array_merge((array) $obj, (array) $std);
+            if (method_exists(__CLASS__, $metodo)) {
+
+                $struct = $this->structure[strtoupper($fields[0])];
+
+                $std = $this->fieldsToStd($fields, $struct);
+
+                $this->$metodo($std);
+            }
         }
-
-        return $obj;
     }
 
     protected function fieldsToStd($dfls, $struct)
@@ -85,6 +67,8 @@ class Parser
         $sfls = explode('|', $struct);
 
         $len = count($sfls) - 1;
+
+        $std = new \stdClass();
 
         for ($i = 1; $i < $len; $i++) {
 
@@ -97,26 +81,115 @@ class Parser
 
             if (!empty($name)) {
 
-                if ($dfls[0] == 'C') {
-
-                    $this->std->prestador->$name = Strings::replaceSpecialsChars($data);
-                } elseif ($dfls[0] == 'E' || $dfls[0] == 'E02') {
-
-                    $this->std->tomador->$name = Strings::replaceSpecialsChars($data);
-                } else {
-
-                    $this->std->$name = Strings::replaceSpecialsChars($data);
-                }
+                $std->$name = Strings::replaceSpecialsChars($data);
             }
         }
 
-        if ($dfls[0] == 'N') {
+        return $std;
+    }
 
-            $this->servicos[] = $dfls;
+    private function aEntity($std)
+    {
+    }
 
-            $this->std->servico = $this->servicos;
-        }
+    private function bEntity($std)
+    {
+        $cnpj = new \stdClass();
 
-        return $this->std;
+        $cnpj = (object) array_merge((array) $cnpj, (array) $std);
+
+        $this->make->buildCpfCnpjPrestador($cnpj);
+
+        $InscricaoMunicipal = new \stdClass();
+
+        $InscricaoMunicipal = (object) array_merge((array) $InscricaoMunicipal, (array) $std);
+
+        $this->make->buildPrestador($InscricaoMunicipal);
+    }
+
+    private function cEntity($std)
+    {
+    }
+
+    private function eEntity($std)
+    {
+        $RazaoSocial = new \stdClass();
+
+        $RazaoSocial = (object) array_merge((array) $RazaoSocial, (array) $std);
+
+        $this->make->buildTomadorServico($RazaoSocial);
+
+        $Endereco = new \stdClass();
+
+        $Endereco = (object) array_merge((array) $Endereco, (array) $std);
+
+        $this->make->buildEndereco($Endereco);
+
+        $Contato = new \stdClass();
+
+        $Contato = (object) array_merge((array) $Contato, (array) $std);
+
+        $this->make->buildContato($Contato);
+    }
+
+    private function e02Entity($std)
+    {
+        $cnpj = new \stdClass();
+
+        $cnpj = (object) array_merge((array) $cnpj, (array) $std);
+
+        $this->make->buildCpfCnpjTomador($cnpj);
+
+        $InscricaoMunicipal = new \stdClass();
+
+        $InscricaoMunicipal = (object) array_merge((array) $InscricaoMunicipal, (array) $std);
+
+        $this->make->buildIdentificacaoTomador($InscricaoMunicipal);
+    }
+
+    private function h01Entity($std)
+    {
+        $IdentificacaoRps = new \stdClass();
+
+        $IdentificacaoRps = (object) array_merge((array) $IdentificacaoRps, (array) $std);
+
+        $this->make->buildIdentificacaoRps($IdentificacaoRps);
+    }
+
+    private function mEntity($std)
+    {
+        $Valores = new \stdClass();
+
+        $Valores = (object) array_merge((array) $Valores, (array) $std);
+
+        $this->make->buildValores($Valores);
+
+        $this->Servico = (object) array_merge((array) $this->Servico, (array) $std);
+    }
+
+    private function nEntity($std)
+    {
+        $this->Servico = (object) array_merge((array) $this->Servico, (array) $std);
+
+        $this->make->buildServico($this->Servico);
+    }
+
+    private function wEntity($std)
+    {
+        $Rps = new \stdClass();
+
+        $Rps = (object) array_merge((array) $Rps, (array) $std);
+
+        $this->make->buildRps($Rps);
+
+        $InfDeclaracaoPrestacaoServico = new \stdClass();
+
+        $InfDeclaracaoPrestacaoServico = (object) array_merge((array) $InfDeclaracaoPrestacaoServico, (array) $std);
+
+        preg_match_all('!\d+!', $std->DataEmissao, $matches);
+
+        $InfDeclaracaoPrestacaoServico->dataEmissao = $matches[0][0] . $matches[0][1] . $matches[0][2];
+
+        $this->make->buildInfDeclaracaoPrestacaoServico($InfDeclaracaoPrestacaoServico);
     }
 }
--- src/Make.php
+++ src/Make.php
@@ -3,11 +3,6 @@
 namespace NFePHP\NFSe\SMARAPD;
 
 use NFePHP\Common\DOMImproved as Dom;
-use NFePHP\Common\Strings;
-use stdClass;
-use RuntimeException;
-use DOMElement;
-use DateTime;
 
 class Make
 {
@@ -26,45 +21,486 @@ class Make
         $this->dom->preserveWhiteSpace = false;
 
         $this->dom->formatOutput = false;
+
+        $this->InfDeclaracaoPrestacaoServico = $this->dom->createElement('InfDeclaracaoPrestacaoServico');
+
+        $this->Rps = $this->dom->createElement('Rps');
+
+        $this->IdentificacaoRps = $this->dom->createElement('IdentificacaoRps');
+
+        $this->Servico = $this->dom->createElement('Servico');
+
+        $this->Valores = $this->dom->createElement('Valores');
+
+        $this->Prestador  = $this->dom->createElement('Prestador');
+
+        $this->CpfCnpjPrestador = $this->dom->createElement('CpfCnpj');
+
+        $this->CpfCnpjTomador = $this->dom->createElement('CpfCnpj');
+
+        $this->TomadorServico = $this->dom->createElement('TomadorServico');
+
+        $this->IdentificacaoTomador = $this->dom->createElement('IdentificacaoTomador');
+
+        $this->Endereco = $this->dom->createElement('Endereco');
+
+        $this->Contato = $this->dom->createElement('Contato');
     }
 
-    public function getXML($std)
+    public function getXML()
     {
 
         if (empty($this->xml)) {
 
-            $this->gerarNota($std);
+            $this->monta();
         }
 
         return $this->xml;
     }
 
-    public function gerarNota($std)
+    public function monta()
     {
+        $this->dom->appendChild($this->InfDeclaracaoPrestacaoServico);
 
-        $root = $this->dom->createElement('nfseCabecMsg');
-        $this->dom->appendChild($root);
+        $this->InfDeclaracaoPrestacaoServico->appendChild($this->Rps);
 
-        $this->dom->addChild(
-            $root,                          // pai
-            "Versão",                       // nome
-            $this->versaoNFSE,              // valor
-            true,                           // se é obrigatorio
-            "Versão do leiaute."            // descrição se der catch
-        );
+        $items = $this->dom->getElementsByTagName('InfDeclaracaoPrestacaoServico');
 
-        $this->dom->addChild(
-            $root,
-            "versaoDados",
-            $std->Versao,
-            true,
-            "Esse campo indica a versão do leiaute XML da estrutura XML informada na área de dados da mensagem."
-        );
+        $firstItem = $items->item(0);
 
+        $firstItem->insertBefore($this->TomadorServico, $firstItem->firstChild);
+
+        $firstItem->insertBefore($this->Prestador, $firstItem->firstChild);
+
+        $firstItem->insertBefore($this->Servico, $firstItem->firstChild);
+
+        $comptc = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('Competencia');
+
+        $comptc = $comptc->item(0);
+
+        $firstItem->insertBefore($comptc, $firstItem->firstChild);
+
+        $firstItem->insertBefore($this->Rps, $firstItem->firstChild);
+
+        $items = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('Rps');
+
+        $firstItem = $items->item(0);
+
+        $firstItem->insertBefore($this->IdentificacaoRps, $firstItem->firstChild);
+
+        $items = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('Servico');
+
+        $firstItem = $items->item(0);
+
+        $firstItem->insertBefore($this->Valores, $firstItem->firstChild);
+
+        $items = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('Prestador');
+
+        $firstItem = $items->item(0);
+
+        $firstItem->insertBefore($this->CpfCnpjPrestador, $firstItem->firstChild);
+
+        $items = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('TomadorServico');
+
+        $firstItem = $items->item(0);
+
+        $firstItem->insertBefore($this->IdentificacaoTomador, $firstItem->firstChild);
+
+        $items = $this->InfDeclaracaoPrestacaoServico->getElementsByTagName('IdentificacaoTomador');
+
+        $firstItem = $items->item(0);
+
+        $firstItem->insertBefore($this->CpfCnpjTomador, $firstItem->firstChild);
+
+        $this->TomadorServico->appendChild($this->Endereco);
+
+        $this->TomadorServico->appendChild($this->Contato);
 
         $this->xml = $this->dom->saveXML();
 
         return $this->xml;
+    }
+
+    public function buildIdentificacaoRps($std)
+    {
+        $this->dom->addChild(
+            $this->IdentificacaoRps,
+            "Numero",
+            $std->Numero,
+            true,
+            "Número do RPS"
+        );
+
+        $this->dom->addChild(
+            $this->IdentificacaoRps,
+            "Serie",
+            $std->Serie,
+            true,
+            "Série do RPS"
+        );
+
+        $this->dom->addChild(
+            $this->IdentificacaoRps,
+            "Tipo",
+            $std->Tipo,
+            true,
+            "Tipo do RPS"
+        );
+    }
+
+    public function buildRps($std)
+    {
+        $this->dom->addChild(
+            $this->Rps,
+            "DataEmissao",
+            $std->DataEmissao,
+            true,
+            "Data de emissão do RPS"
+        );
+
+        $this->dom->addChild(
+            $this->Rps,
+            "Status",
+            $std->Status,
+            true,
+            "Situação do RPS"
+        );
+    }
+
+    public function buildValores($std)
+    {
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorServicos",
+            $std->ValorServicos,
+            true,
+            "Valor do serviço"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorDeducoes",
+            $std->ValorDeducoes,
+            true,
+            "Valor total das deduções"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorPis",
+            $std->ValorPis,
+            true,
+            "Valor do PIS"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorCofins",
+            $std->ValorCofins,
+            true,
+            "Valor do CONFINS"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorInss",
+            $std->ValorInss,
+            true,
+            "Valor do INSS"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorIr",
+            $std->ValorIr,
+            true,
+            "Valor do imposto de renda"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorCsll",
+            $std->ValorCsll,
+            true,
+            "Valor do CSLL"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "OutrasRetencoes",
+            $std->ValorOutrasRetencoes,
+            true,
+            "Valor total de outras retenções"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "ValorIss",
+            $std->ValorIss,
+            true,
+            "Valor do ISS quando informado pelo prestador"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "Aliquota",
+            $std->Aliquota,
+            true,
+            "Alíquota do ISS quando informado pelo prestador"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "DescontoIncondicionado",
+            $std->DescontoIncondicionado,
+            true,
+            "Valor do desconto condicionado"
+        );
+
+        $this->dom->addChild(
+            $this->Valores,
+            "DescontoCondicionado",
+            $std->DescontoCondicionado,
+            true,
+            "Valor do desconto incondicionado"
+        );
+    }
+
+    public function buildServico($std)
+    {
+
+        $this->dom->addChild(
+            $this->Servico,
+            "IssRetido",
+            $std->IssRetido,
+            true,
+            "ISS retido (S/N)"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "ItemListaServico",
+            $std->ItemListaServico,
+            true,
+            "Subitem do serviço prestado"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "CodigoCnae",
+            $std->CodigoCnae,
+            true,
+            "Código CNAE"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "CodigoTributacaoMunicipio",
+            $std->CodigoTributacaoMunicipio,
+            true,
+            "Código de tributação do município"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "Discriminacao",
+            $std->Discriminacao,
+            true,
+            "Discriminação do serviço"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "CodigoMunicipio",
+            $std->CodigoMunicipio,
+            true,
+            "Código do município da prestação do serviço"
+        );
+
+        $this->dom->addChild(
+            $this->Servico,
+            "ExigibilidadeISS",
+            $std->CodigoMunicipio,
+            true,
+            "Exigibilidade do ISS"
+        );
+    }
+
+    public function buildCpfCnpjPrestador($std)
+    {
+
+        $this->dom->addChild(
+            $this->CpfCnpjPrestador,
+            "Cnpj",
+            $std->Cnpj,
+            true,
+            "Número do Cnpj"
+        );
+    }
+
+    public function buildCpfCnpjTomador($std)
+    {
+
+        $this->dom->addChild(
+            $this->CpfCnpjTomador,
+            "Cnpj",
+            $std->Cnpj,
+            true,
+            "Número do Cnpj"
+        );
+    }
+
+    public function buildPrestador($std)
+    {
+
+        $this->dom->addChild(
+            $this->Prestador,
+            "InscricaoMunicipal",
+            $std->Cnpj,
+            true,
+            "Inscrição Municipal da empresa/pessoa"
+        );
+    }
+
+    public function buildIdentificacaoTomador($std)
+    {
+
+        $this->dom->addChild(
+            $this->IdentificacaoTomador,
+            "InscricaoMunicipal",
+            $std->InscricaoMunicipal,
+            true,
+            "Inscrição Municipal da empresa/pessoa"
+        );
+    }
+
+    public function buildTomadorServico($std)
+    {
+
+        $this->dom->addChild(
+            $this->TomadorServico,
+            "RazaoSocial",
+            $std->RazaoSocial,
+            true,
+            "Razão Social do tomador do serviço"
+        );
+    }
+
+    public function buildEndereco($std)
+    {
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Endereco",
+            $std->Endereco,
+            true,
+            "Tipo e nome do logradouro"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Numero",
+            $std->Numero,
+            true,
+            "Número do imóvel"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Complemento",
+            $std->Complemento,
+            true,
+            "Complemento do Endereço"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Bairro",
+            $std->Bairro,
+            true,
+            "Nome do bairro"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "CodigoMunicipio",
+            $std->CodigoMunicipio,
+            true,
+            "Código da cidade"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Uf",
+            $std->Uf,
+            true,
+            "Sigla do estado"
+        );
+
+        $this->dom->addChild(
+            $this->Endereco,
+            "Cep",
+            $std->Cep,
+            true,
+            "CEP da localidade"
+        );
+    }
+
+    public function buildContato($std)
+    {
+
+        $this->dom->addChild(
+            $this->Contato,
+            "Telefone",
+            $std->Telefone,
+            true,
+            "Número do telefone"
+        );
+
+        $this->dom->addChild(
+            $this->Contato,
+            "Email",
+            $std->Email,
+            true,
+            "Endereço eletrônico (email)"
+        );
+    }
+
+    public function buildInfDeclaracaoPrestacaoServico($std)
+    {
+
+        $this->dom->addChild(
+            $this->InfDeclaracaoPrestacaoServico,
+            "Competencia",
+            $std->dataEmissao,
+            true,
+            "Dia, mês e ano da prestação de serviço (AAAAMMDD)"
+        );
+
+        $this->dom->addChild(
+            $this->InfDeclaracaoPrestacaoServico,
+            "RegimeEspecialTributacao",
+            $std->RegimeEspecialTributacao,
+            true,
+            "Identificação do regime especial de tributação"
+        );
+
+        $this->dom->addChild(
+            $this->InfDeclaracaoPrestacaoServico,
+            "OptanteSimplesNacional",
+            $std->OptanteSimplesNacional,
+            true,
+            "Informação se optante pelo Simples Nacional (S/N)"
+        );
+
+        $this->dom->addChild(
+            $this->InfDeclaracaoPrestacaoServico,
+            "IncentivoFiscal",
+            $std->RegimeEspecialTributacao,
+            true,
+            "Informação se o prestador é incentivador fiscal"
+        );
     }
 
     public function cancelarNota($std)
